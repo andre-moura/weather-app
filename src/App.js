4231627dd1c3379a4606eec56b136d994a667238
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-09-25 09:58:53 -0300

    Application fetching Open Weather API and getting temperature in Celsius

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+
+  const apiKey = ''
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("")
+  
+  const getWeather = (event) => {
+    if (event.key === "Enter") {  
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`).then(
+        response => response.json()
+      ).then(
+        data => {
+          setWeatherData(data)
+          setCity("")
+          console.log(weatherData)
+          console.log(data)
+        }
+      )
+    }
+  }
+  
   return (
-    <div className="App">
-     
+    <div className="weather-container">
+      <input className="weather-input" placeholder="Enter City..." onChange={e => setCity(e.target.value)} value={city} onKeyPress={getWeather}/>
+      {
+      typeof weatherData.main === "undefined" ? (
+        <div>
+          <p>
+            Welcome to Weather World! Enter in a city to get the weather.
+          </p>
+        </div>
+      ) : (
+        <div>
+          <p>{weatherData.name}</p>
+          <p>{Math.round(weatherData.main.temp)}°C</p>
+          <p>{weatherData.weather[0].main}</p>
+        </div>
+      )}
+      {weatherData.cod === "404" ? (
+        <p>
+          City not found!
+        </p>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
