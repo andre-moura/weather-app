5e66a3e91cc49b951f2744e611bd93d326316687
andre-moura <61800450+andre-moura@users.noreply.github.com>
2023-03-10 19:29:27 -0300

    Allowing application to get data when the user enters the application, or when the user types a city name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react'
+import Geolocation from 'react-geolocation';
 
 import clearIcon from '../src/assets/img/clear.png'
 import rainyIcon from '../src/assets/img/rainy.png'
@@ -11,11 +12,43 @@ import thunderIcon from '../src/assets/img/thunderstorm.png'
 
 function App() {
   const apiKey = process.env.REACT_APP_API_KEY;
+  const baseURL = `https://api.openweathermap.org/data/2.5/weather`
+
   const [weatherData, setWeatherData] = useState([{}]);
   const options = { weekday: 'long', day: 'numeric', month: 'short' };
 
   const [currentTime, setCurrentTime] = useState('');
   const [currentDate, setCurrentDate] = useState('');
+
+  const [typedCity, setTypedCity] = useState('');
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          const apiUrl = baseURL +`?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+          fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+              setWeatherData(data)
+              console.log(data)
+            })
+            .catch((error) => console.log(error));
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -29,48 +62,47 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
-  const [city, setCity] = useState('');
-  const iconStyle = {
-    width: '75px'
-  }
-  
   const getWeather = (event) => {
     if (event.key === 'Enter') {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`).then(
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const apiUrl = baseURL +`?q=${typedCity}&appid=${apiKey}&units=metric`;
+      fetch(apiUrl).then(
         response => response.json()
       ).then(
         data => {
           setWeatherData(data)
-          setCity('')
-          console.log(weatherData)
+          setCountry(data.sys.country);
+          setCity(data.name);
           console.log(data)
         }
       )
     }
   }
 
-  function WeatherIcon(main) {
-    switch (toString(main)) {
-      case 'Clouds':
-        return cloudyIcon;
-      case 'Clear':
-        return clearIcon;
-      case 'Rain':
-        return rainyIcon;
-      case 'Snow':
-        return snowIcon;
-      case 'Thunderstorm':
-        return thunderIcon;
-      default:
-        return scatterCloudIcon;
-    }
-  }
+  // function WeatherIcon(main) {
+  //   switch (toString(main)) {
+  //     case 'Clouds':
+  //       return cloudyIcon;
+  //     case 'Clear':
+  //       return clearIcon;
+  //     case 'Rain':
+  //       return rainyIcon;
+  //     case 'Snow':
+  //       return snowIcon;
+  //     case 'Thunderstorm':
+  //       return thunderIcon;
+  //     default:
+  //       return scatterCloudIcon;
+  //   }
+  // }
 
   return (
     <div className='weather-container'>
-      <input className='weather-input' placeholder='Enter City...' onChange={e => setCity(e.target.value)} value={city} onKeyPress={getWeather} />
+      <input className='weather-input' placeholder='Enter City...' onChange={e => setTypedCity(e.target.value)} value={typedCity} onKeyDown={getWeather} />
+      <h1>{currentTime}</h1>
+      <h3>{currentDate}</h3>
       {
-        typeof weatherData.main !== 'undefined' ? (
+        typeof weatherData.main === 'undefined' ? (
           <div>
             <p>
               Welcome to Weather App! Enter in a city to get the weather.
@@ -78,10 +110,6 @@ function App() {
           </div>
         ) : (
           <div className='container'>
-            <h1>
-              {currentTime}
-            </h1>
-            <h3>{currentDate}</h3>
             <div className='days-container'>
               <div className='box'>
                 <div className='day'>Mon</div>
